dd572df18a953cbce228d0baaef03a72ec77d4d3
ADARBEH <retaad99@gmail.com>
2022-09-05 23:40:28 +0300

    first commit

---
+++ src/components/Person.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            age: '',
+            gender: ''
+        }
+    }
+
+    changeHandel = (e) => {
+        this.setState({[e.target.name]: e.target.value })
+    }
+
+    submitHandle = (e) => {
+        e.preventDefault();
+        const dd = axios.post('http://localhost:3000/person', this.state)
+            .then(response => {
+                console.log(response.data)
+                return response.data
+
+            })
+            console.log(dd.Object)
+
+
+            .Catsh(error => {
+                console.log(error)
+            }
+            )
+    }
+
+    render() {
+        const { name, age, gender } = this.state
+        return (
+            <>
+            
+            <div>
+                <form onSubmit={this.submitHandle}>
+                    <div>
+                        <input type="text" name="name" value={name} onChange={this.changeHandel} />
+                    </div>
+
+                    <div>
+                        <input type="text" name="age" value={age} onChange={this.changeHandel} />
+                    </div>
+                    <div>
+                        <input type="text" name="gender" value={gender} onChange={this.changeHandel} />
+                    </div>
+                    <button type="submit">Submit</button>
+
+                </form>
+            </div>
+            <div>
+                <h1>YOUR INFO </h1>
+                <h3>name: {name}</h3>
+                <h3>age: {age}</h3>
+                <h3>gender:{gender}</h3>
+            </div>
+
+            </>
+        );
+    }
+}
+
+export default Person;
